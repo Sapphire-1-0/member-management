d5c836fae7939e545e99d860450978247ea48b09
vbalaji215-dev <vbalaji215@outlook.com>
2025-03-01 22:13:28 -0500

    Updated application.yaml

--- src/main/java/com/brihaspathee/sapphire/config/WebMvcConfig.java
+++ src/main/java/com/brihaspathee/sapphire/config/WebMvcConfig.java
@@ -1,11 +1,14 @@
 package com.brihaspathee.sapphire.config;
 
 import com.brihaspathee.sapphire.interceptors.UserContextInterceptor;
+import com.netflix.discovery.converters.Auto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Created in Intellij IDEA
@@ -24,19 +27,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
      * A final instance of the UserContextInterceptor that is used to intercept
      * HTTP requests and perform pre-processing logic, such as injecting or validating
      * user context.
-     *
+     * <p>
      * This interceptor is designed to operate during the lifecycle of a request,
      * ensuring that user-related data, such as user ID or username, is extracted
      * and set in a thread-safe manner for downstream processing. It leverages the
      * UserContext class to associate user-specific information with the current thread.
-     *
+     * <p>
      * The UserContextInterceptor is executed for each request within the web application
      * and is pivotal for maintaining contextual information about the user for the
      * duration of the request lifecycle.
-     *
+     * <p>
      * Being a final field ensures its immutability, thereby providing a consistent
      * and reliable mechanism for pre-request processing in the web application.
      */
+
     private final UserContextInterceptor userContextInterceptor;
 
     /**
